ac7db691b3c736fe261348807bf9d8bc2f0301c0
artjmaria <artjmaria@gmail.com>
2023-01-09 11:19:28 -0600

    final changes on last functions

--- main.js
+++ main.js
@@ -48,83 +48,93 @@ const convertToNumber = () => {
 // * String
 
 const dataTypes = () => {
-    const dataInput = document.getElementById('dataInputted').value;
+  const dataInput = document.getElementById('dataInputted').value;
 
-    if (dataInput === 'true' || dataInput === 'false') {
-      return document.getElementById("dataType").innerHTML = 'Boolean'
-    };
-    if (dataInput === 'null') {
-      return document.getElementById("dataType").innerHTML = 'null'
-    };
-    if (dataInput === 'undefined') {
-      return document.getElementById("dataType").innerHTML = 'undefined'
-    };
-    if (isNaN(dataInput)) {
-      return document.getElementById("dataType").innerHTML = 'NaN'
-    } else {
-      return document.getElementById("dataType").innerHTML = 'Number'
-    };
+  if (dataInput === 'true' || dataInput === 'false') {
+    return document.getElementById("dataType").innerHTML = 'Boolean'
   };
+  if (dataInput === 'null') {
+    return document.getElementById("dataType").innerHTML = 'null'
+  };
+  if (dataInput === 'undefined') {
+    return document.getElementById("dataType").innerHTML = 'undefined'
+  };
+  if (isNaN(dataInput)) {
+    return document.getElementById("dataType").innerHTML = 'NaN'
+  } else {
+    return document.getElementById("dataType").innerHTML = 'Number'
+  };
+};
 
 // ====================================================================================================================================================
 
 
-    // Write a JavaScript program that adds 2 numbers together.
+// Write a JavaScript program that adds 2 numbers together.
 
-    const summation = () => {
-      const numInput1 = parseInt(document.getElementById('num1').value)
-      const numInput2 = parseInt(document.getElementById('num2').value)
-      let sumNumbers = numInput1 + numInput2
-      return document.getElementById("results").innerHTML = sumNumbers;
+const summation = () => {
+  const numInput1 = parseInt(document.getElementById('num1').value)
+  const numInput2 = parseInt(document.getElementById('num2').value)
+  let sumNumbers = numInput1 + numInput2
+  return document.getElementById("results").innerHTML = sumNumbers;
+}
+
+// ====================================================================================================================================================
+
+
+// Write a JavaScript program that runs only when 2 things are true.
+
+
+const bothTruthy = () => {
+  const userInput1 = document.getElementById('input1').value
+  const userInput2 = document.getElementById('input2').value
+  if (userInput1 && userInput2) {
+    document.getElementById("bothTrue").innerHTML = `${userInput1} & ${userInput2}, are truthy inputs!`
   }
+}
 
 // ====================================================================================================================================================
 
 
-    // Write a JavaScript program that runs only when 2 things are true.
+// Write a JavaScript program that runs when 1 of 2 things are true.
+
+const oneTruthy = () => {
+  const userInput1 = document.getElementById('inputUno').value
+  const userInput2 = document.getElementById('inputDos').value
+  if (userInput1 || userInput2) {
+    document.getElementById("oneTruthyVal").innerHTML = `${userInput1} ${userInput2}, at least one of them is truthy!`
+  }
+}
+
+// ====================================================================================================================================================
 
 
-    const bothTruthy = () => {
-      const userInput1 = document.getElementById('input1').value
-      const userInput2 = document.getElementById('input2').value
-      if (userInput1 && userInput2) {
-        document.getElementById("bothTrue").innerHTML = `${userInput1} & ${userInput2}, are truthy inputs!`
-      }
-    }
-  
+// Write a JavaScript program that runs when both things are not true.  
 
 
-    // Write a JavaScript program that runs when 1 of 2 things are true.
+const bothFalsy = () => {
+  const userInput1 = document.getElementById('inputA').value
+  const userInput2 = document.getElementById('inputB').value
+  if (!userInput1 && !userInput2) {
+    document.getElementById("bothFalsyVal").innerHTML = `Nothing was entered which results in empty strings, so it's falsy!`
+  }
+}
 
-    const oneTruthy = () => {
-      const userInput1 = document.getElementById('inputUno').value
-      const userInput2 = document.getElementById('inputDos').value
-      if (userInput1 || userInput2) {
-        document.getElementById("oneTruthyVal").innerHTML = `${userInput1} ${userInput2}`
-      }
-    }
+// ***************************
+//         PART TWO
+// ***************************
 
-
-
-
-    // Write a JavaScript program that runs when both things are not true.  
-
-    // ***************************
-    //         PART TWO
-    // ***************************
-
-    // 1. download Live-Server by Ritwick Dey, 
-    // 2. reload VS Code, 
-    // 3. click the "Go Live" button
-    // 4. Go local host 5500 or http://127.0.0.1:5500/index.html to see your web page
-    // 5. Or go use the `npm start` command and navigate to localhost:8080 (ctrl + C to close)
-    // 6. go to `index.html` 
-    // 7. create inputs, buttons and event listeners that render the code blocks you built above to the DOM.
+// 1. download Live-Server by Ritwick Dey, 
+// 2. reload VS Code, 
+// 3. click the "Go Live" button
+// 4. Go local host 5500 or http://127.0.0.1:5500/index.html to see your web page
+// 5. Or go use the `npm start` command and navigate to localhost:8080 (ctrl + C to close)
+// 6. go to `index.html` 
+// 7. create inputs, buttons and event listeners that render the code blocks you built above to the DOM.
 
 
 
 
-    // Additional Resources
-    // Video1: https://player.vimeo.com/video/377147232
-    // Video2: https://www.youtube.com/embed/bkvH28PXLWc
-    // Video3: https://www.youtube.com/embed/TrGI9Yki-24
+// Additional Resources
+// Video1: https://player.vimeo.com/video/377147232
+// Video2: https://www.youtube.com/embed/bkvH28PXLWc
+// Video3: https://www.youtube.com/embed/TrGI9Yki-24
